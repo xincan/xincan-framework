e6c752ca102ab68887ae41e6cf900ca8f2c5ffd6
xincan <alittlexincan@sina.com>
2020-07-29 17:30:11 +0800

    优化公共实体包

---
+++ xincan-entity/src/main/java/cn/com/xincan/xincanframework/entity/XincanEntityApplication.java
@@ -0,0 +1,20 @@
+package cn.com.xincan.xincanframework.entity;
+
+/**
+ * copyright (C), 2020, 心灿基础架构
+ *
+ * @author JiangXincan
+ * @version 0.0.1
+ * @program xincan-framework
+ * @description 公共实体包
+ * @create 2020/7/20 16:46
+ * <author>                <time>                  <version>                   <description>
+ * Jiangxincan         2020/7/20 16:46             0.0.1                         公共实体包
+ */
+public class XincanEntityApplication {
+
+    public static void main(String[] args) {
+        System.out.println("自定义公共实体启动类");
+    }
+
+}
