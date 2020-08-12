7e2dbc1f6f4bd9b2a67dc20d102a22e65cc57dae
xincan <alittlexincan@sina.com>
2020-08-12 16:06:23 +0800

    采用java方式配置mybatis-plus

--- xincan-user/src/main/java/cn/com/xincan/xincanframework/config/mybatis/MyBatisPlusConfig.java
+++ xincan-user/src/main/java/cn/com/xincan/xincanframework/config/mybatis/MyBatisPlusConfig.java
@@ -1,10 +1,12 @@
 package cn.com.xincan.xincanframework.config.mybatis;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+
+
+
 
 /**
  * copyright (C), 2019, 心灿基础架构
@@ -27,17 +32,19 @@ import javax.sql.DataSource;
  * <author>                <time>                  <version>                   <description>
  * JiangXincan         2019/12/20 15:27             0.0.1                         MyBatisPlus 配置
  */
-@EnableTransactionManagement
 @Configuration
+@EnableTransactionManagement
 @MapperScan("cn.com.xincan.xincanframework.server.*.mapper")
 public class MyBatisPlusConfig {
+
+    private static final String MAPPER_PATTERN = "classpath:mapper/**/*.xml";
 
     @Resource(name = "routingDataSource")
     private DataSource routingDataSource;
 
 
     /**
-     * MyBatisPlus配置分页
+     * MyBatisPlus 配置分页
      * @author JiangXincan
      * @date 2020/1/7 10:22
      * @return com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor
@@ -47,16 +54,78 @@ public class MyBatisPlusConfig {
         return  new PaginationInterceptor();
     }
 
+    /**
+     *  SQL 工厂配置
+     * @param mybatisConfiguration  mybatis 配置类
+     * @param globalConfig 全局配置
+     * @author JiangXincan
+     * @date 2020/8/12 16:02
+     * @return org.apache.ibatis.session.SqlSessionFactory
+     */
     @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(routingDataSource);
-        return sqlSessionFactoryBean.getObject();
+    public SqlSessionFactory sqlSessionFactory(
+            @Qualifier("mybatisConfiguration") MybatisConfiguration mybatisConfiguration,
+            @Qualifier("globalConfig") GlobalConfig globalConfig
+    ) throws Exception {
+        PathMatchingResourcePatternResolver pathMatchingResourcePatternResolver = new PathMatchingResourcePatternResolver();
+        MybatisSqlSessionFactoryBean mybatisSqlSessionFactoryBean = new MybatisSqlSessionFactoryBean();
+        mybatisSqlSessionFactoryBean.setDataSource(routingDataSource);
+        mybatisSqlSessionFactoryBean.setConfiguration(mybatisConfiguration);
+        mybatisSqlSessionFactoryBean.setGlobalConfig(globalConfig);
+        mybatisSqlSessionFactoryBean.setMapperLocations(pathMatchingResourcePatternResolver.getResources(MAPPER_PATTERN));
+        return mybatisSqlSessionFactoryBean.getObject();
     }
 
+    /**
+     *  MyBatisPlus 配置
+     * @author JiangXincan
+     * @date 2020/8/12 16:01
+     * @return com.baomidou.mybatisplus.core.MybatisConfiguration
+     */
+    @Bean
+    public MybatisConfiguration mybatisConfiguration(){
+        MybatisConfiguration mybatisConfiguration = new MybatisConfiguration();
+        mybatisConfiguration.setMapUnderscoreToCamelCase(true);
+        mybatisConfiguration.setCallSettersOnNulls(false);
+        mybatisConfiguration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+        return mybatisConfiguration;
+    }
+
+    /**
+     *  MyBatisPlus GlobalConfig 全局配置
+     * @param dbConfig 数据库配置类
+     * @author JiangXincan
+     * @date 2020/8/12 15:58
+     * @return com.baomidou.mybatisplus.core.config.GlobalConfig
+     */
+    @Bean
+    public GlobalConfig globalConfig(@Qualifier("dbConfig") GlobalConfig.DbConfig dbConfig){
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setDbConfig(dbConfig);
+        return globalConfig;
+    }
+
+    /**
+     * MyBatisPlus DbConfig 数据库配置
+     * @author JiangXincan
+     * @date 2020/8/12 16:00
+     * @return com.baomidou.mybatisplus.core.config.GlobalConfig.DbConfig
+     */
+    @Bean
+    public GlobalConfig.DbConfig dbConfig(){
+        GlobalConfig.DbConfig dbConfig = new GlobalConfig.DbConfig();
+        dbConfig.setIdType(IdType.ASSIGN_UUID);
+        return dbConfig;
+    }
+
+    /**
+     *  配置事务管理器
+     * @author JiangXincan
+     * @date 2020/8/12 15:59
+     * @return org.springframework.transaction.PlatformTransactionManager
+     */
     @Bean
     public PlatformTransactionManager platformTransactionManager() {
         return new DataSourceTransactionManager(routingDataSource);
     }
-
 }
