7a2aacdce4d1a70828c7287226bb58a95e717d04
xincan <alittlexincan@sina.com>
2020-08-25 11:39:09 +0800

    优化数据源配置日志信息

--- sql/nacos-test.sql
+++ sql/nacos-test.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 50731
  Source Host           : 192.168.3.50:3306
- Source Schema         : nacos-test
+ Source Schema         : nacos-dev
 
  Target Server Type    : MySQL
  Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 29/07/2020 09:35:38
+ Date: 29/07/2020 09:35:28
 */
 
 SET NAMES utf8mb4;
@@ -24,13 +24,18 @@ DROP TABLE IF EXISTS `order`;
 CREATE TABLE `order`  (
   `id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '订单ID',
   `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单名称',
-  `ddress` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单地址（收货地址）',
+  `address` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单地址（收货地址）',
   `user_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单人ID（收货人ID）',
   `description` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL COMMENT '订单说明（附加信息）',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '订单时间',
   `end_time` datetime(0) NULL DEFAULT NULL COMMENT '收货时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+INSERT INTO `order` VALUES ('1285522130867376129', '购买佛珠', '北京市房山区', 'UUID', '注意物件不能轻易碰撞', '2020-12-12 15:23:23', '2020-12-12 15:23:23');
 
 -- ----------------------------
 -- Table structure for user
@@ -45,5 +50,10 @@ CREATE TABLE `user`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1285759156342562818', '张三', 'zhangsan', '123456', '18503455667', '2020-12-12 15:23:23');
 
 SET FOREIGN_KEY_CHECKS = 1;
