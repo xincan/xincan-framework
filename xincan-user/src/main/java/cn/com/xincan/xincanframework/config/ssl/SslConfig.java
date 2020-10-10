ed5d10517ba18bab7fbb0ab3ead27b6e5cb7e5ad
xincan <alittlexincan@sina.com>
2020-10-10 16:47:38 +0800

    用户管理增加https访问

---
+++ xincan-user/src/main/java/cn/com/xincan/xincanframework/config/ssl/SslConfig.java
@@ -0,0 +1,44 @@
+package cn.com.xincan.xincanframework.config.ssl;
+
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import io.undertow.servlet.api.SecurityConstraint;
+import io.undertow.servlet.api.SecurityInfo;
+import io.undertow.servlet.api.TransportGuaranteeType;
+import io.undertow.servlet.api.WebResourceCollection;
+import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * copyright (C), 2020, 北京同创永益科技发展有限公司
+ *
+ * @author JiangXincan
+ * @version 1.0
+ * <author>                <time>                  <version>                   <description>
+ * Jiangxincan         2020/10/10 15:26             1.0                         https链接转发配置
+ * @program xincan-framework
+ * @description https链接转发配置
+ * @create 2020/10/10 15:26
+ */
+@Configuration
+public class SslConfig {
+
+    @Bean
+    public UndertowServletWebServerFactory undertowServletWebServerFactory() {
+        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+        // 这段就可以可以转换为http2
+        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_HTTP2, true));
+        //这段可以增加http重定向，如果只需要http2的话下面的代码可以去掉
+//        factory.addBuilderCustomizers(builder -> builder.addHttpListener(8000, "0.0.0.0"));
+
+        //下面这段是将http的8000端口重定向到https的8443端口上
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addSecurityConstraint(new SecurityConstraint()
+                .addWebResourceCollection(new WebResourceCollection()
+                .addUrlPattern("/*")) .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
+                .setConfidentialPortManager(exchange -> 8000));
+        return factory;
+    }
+}
