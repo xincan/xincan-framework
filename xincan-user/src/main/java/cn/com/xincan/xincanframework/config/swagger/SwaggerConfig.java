2ea548dd007af081fee7dc461e9c4f21d74fd2a5
xincan <alittlexincan@sina.com>
2020-10-13 17:45:27 +0800

    优化用户管理swagger

--- xincan-user/src/main/java/cn/com/xincan/xincanframework/config/swagger/SwaggerConfig.java
+++ xincan-user/src/main/java/cn/com/xincan/xincanframework/config/swagger/SwaggerConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -55,10 +56,12 @@ public class SwaggerConfig {
     }
 
     private ApiInfo apiInfo() {
+
         return new ApiInfoBuilder()
-                .title(applicationName + " RESTFUL APIs")
-                .description(applicationName + " RESTFUL APIs")
-                .termsOfServiceUrl("http://www.github.com/xincan")
+                .title(applicationName.toUpperCase() + " RESTFUL APIS")
+                .contact(new Contact("xincan","http://www.github.com/xincan", "alittlexincan@163.com"))
+                .description("用户信息管理")
+                .termsOfServiceUrl("/user/*")
                 .version("0.0.1")
                 .build();
     }
