290233a0c4ca79fbf048e5abe0b0c08a22f6e0e0
xincan <alittlexincan@sina.com>
2020-10-13 17:45:53 +0800

    用户管理增加https访问

--- xincan-user/src/main/java/cn/com/xincan/xincanframework/config/ssl/SslConfig.java
+++ xincan-user/src/main/java/cn/com/xincan/xincanframework/config/ssl/SslConfig.java
@@ -6,6 +6,7 @@ import io.undertow.servlet.api.SecurityConstraint;
 import io.undertow.servlet.api.SecurityInfo;
 import io.undertow.servlet.api.TransportGuaranteeType;
 import io.undertow.servlet.api.WebResourceCollection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -17,13 +18,16 @@ import org.springframework.context.annotation.Configuration;
  * @author JiangXincan
  * @version 1.0
  * <author>                <time>                  <version>                   <description>
- * Jiangxincan         2020/10/10 15:26             1.0                         https链接转发配置
+ * Jiangxincan         2020/10/10 15:26             1.0                         采用Undertow作为服务器,支持Https服务配置
  * @program xincan-framework
- * @description https链接转发配置
+ * @description 采用Undertow作为服务器,支持Https服务配置
  * @create 2020/10/10 15:26
  */
 @Configuration
 public class SslConfig {
+
+    @Value("${server.port}")
+    private Integer serverPort;
 
     @Bean
     public UndertowServletWebServerFactory undertowServletWebServerFactory() {
@@ -33,12 +37,12 @@ public class SslConfig {
         //这段可以增加http重定向，如果只需要http2的话下面的代码可以去掉
 //        factory.addBuilderCustomizers(builder -> builder.addHttpListener(8000, "0.0.0.0"));
 
-        //下面这段是将http的8000端口重定向到https的8443端口上
+        //下面这段是将http的8000端口重定向到https的8000端口上
         factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addSecurityConstraint(new SecurityConstraint()
                 .addWebResourceCollection(new WebResourceCollection()
                 .addUrlPattern("/*")) .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
                 .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
-                .setConfidentialPortManager(exchange -> 8000));
+                .setConfidentialPortManager(exchange -> serverPort));
         return factory;
     }
 }
