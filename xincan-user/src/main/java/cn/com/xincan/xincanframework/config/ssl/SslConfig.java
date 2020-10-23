944b559c449aa3ed30e997502b84e77198dd769e
xincan <alittlexincan@sina.com>
2020-10-23 10:11:06 +0800

    优化用户管理微服务ssl，https访问配置

--- xincan-user/src/main/java/cn/com/xincan/xincanframework/config/ssl/SslConfig.java
+++ xincan-user/src/main/java/cn/com/xincan/xincanframework/config/ssl/SslConfig.java
@@ -29,6 +29,18 @@ public class SslConfig {
     @Value("${server.port}")
     private Integer serverPort;
 
+    /**
+     * @author jinhaoxun
+     * @description 采用Undertow作为服务器。
+     * Undertow是一个用java编写的、灵活的、高性能的Web服务器，提供基于NIO的阻塞和非阻塞API，特点：
+     * 非常轻量级，Undertow核心瓶子在1Mb以下。它在运行时也是轻量级的，有一个简单的嵌入式服务器使用少于4Mb的堆空间。
+     * 支持HTTP升级，允许多个协议通过HTTP端口进行多路复用。
+     * 提供对Web套接字的全面支持，包括JSR-356支持。
+     * 提供对Servlet 3.1的支持，包括对嵌入式servlet的支持。还可以在同一部署中混合Servlet和本机Undertow非阻塞处理程序。
+     * 可以嵌入在应用程序中或独立运行，只需几行代码。
+     * 通过将处理程序链接在一起来配置Undertow服务器。它可以对各种功能进行配置，方便灵活。
+     * @return ServletWebServerFactory
+     */
     @Bean
     public UndertowServletWebServerFactory undertowServletWebServerFactory() {
         UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
@@ -40,7 +52,8 @@ public class SslConfig {
         //下面这段是将http的8000端口重定向到https的8000端口上
         factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addSecurityConstraint(new SecurityConstraint()
                 .addWebResourceCollection(new WebResourceCollection()
-                .addUrlPattern("/*")) .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
+                .addUrlPattern("/*"))
+                .setTransportGuaranteeType(TransportGuaranteeType.CONFIDENTIAL)
                 .setEmptyRoleSemantic(SecurityInfo.EmptyRoleSemantic.PERMIT))
                 .setConfidentialPortManager(exchange -> serverPort));
         return factory;
