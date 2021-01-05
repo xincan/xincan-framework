9679d5064d4f00c6f7dcd57be575ed27e03cc7c5
jiangxincan <jiangxincan@hatech.com.cn>
2021-01-05 15:10:24 +0800

    订单微服务增加sentinel+nacos存储sentinel配置信息

--- xincan-order/src/main/java/cn/com/xincan/xincanframework/server/order/controller/OrderController.java
+++ xincan-order/src/main/java/cn/com/xincan/xincanframework/server/order/controller/OrderController.java
@@ -8,6 +8,7 @@ import cn.com.xincan.xincanframework.server.order.service.IOrderService;
 import cn.com.xincan.xincanframework.utils.response.ResponseObject;
 import cn.com.xincan.xincanframework.utils.response.ResponseResult;
 import cn.com.xincan.xincanframework.utils.response.ResponseResultBody;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -49,6 +50,7 @@ public class OrderController {
 
     @ApiOperation(value = "根据订单ID查询订单信息", httpMethod = "GET", notes = "根据订单ID查询订单信息")
     @GetMapping("/{id}")
+    @SentinelResource("/order/{id}")
     public OrderSearchVo findOrderById(
             @ApiParam(name = "id", value = "订单ID", required = true, example = "1334332759472676865")
             @Length(message = "订单ID长度应为{max}位", min = 19, max = 19)
@@ -60,6 +62,7 @@ public class OrderController {
 
     @ApiOperation(value = "根据用户ID查询订单信息", httpMethod = "GET", notes = "根据用户ID查询订单信息")
     @GetMapping("/user/{id}")
+    @SentinelResource("/order/user/{id}")
     public OrderSearchVo findOrderByUserId(
             @ApiParam(name = "id", value = "用户ID", required = true, example = "1334332594997260290")
             @Length(message = "用户ID长度应为{max}位", min = 19, max = 19)
@@ -71,6 +74,7 @@ public class OrderController {
 
     @ApiOperation(value = "查询订单信息（分页）", httpMethod = "POST", notes = "根据参数列表查询部分订单列表信息")
     @PostMapping
+    @SentinelResource("/post/order")
     public ResponseObject<List<OrderSearchVo>> page(@ApiParam @Validated @RequestBody OrderSearchDto orderSearchDto) {
         Page<OrderSearchVo> page = orderService.page(orderSearchDto);
         return ResponseResult.success(page.getTotal(), page.getRecords());
@@ -78,18 +82,21 @@ public class OrderController {
 
     @ApiOperation(value = "新增订单信息", httpMethod = "PUT", notes = "新增订单信息")
     @PutMapping
+    @SentinelResource("/put/order")
     public OrderSearchVo save(@ApiParam @Validated OrderSaveDto orderSaveDto) {
         return orderService.save(orderSaveDto);
     }
 
     @ApiOperation(value = "修改订单信息", httpMethod = "PATCH", notes = "根据参数列表修改订单信息")
     @PatchMapping
+    @SentinelResource("/patch/order")
     public OrderSearchVo patch(@ApiParam @Validated OrderPatchDto orderPatchDto) {
         return orderService.patch(orderPatchDto);
     }
 
     @ApiOperation(value = "删除订单信息", httpMethod = "DELETE", notes = "根据ID删除订单信息")
     @DeleteMapping
+    @SentinelResource("/delete/order")
     public Integer delete(
             @ApiParam(name = "id", value = "订单ID", required = true, example = "1334332759472676865")
             @NotBlank(message = "订单ID不能为空")
